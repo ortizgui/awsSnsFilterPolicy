4857d622508a0b4c4402ddc8a7875a69f0d6df7b
Guilherme Ortiz <code@guilhermeortiz.net>
2021-08-09 00:18:19 -0300

    Add consumer-B

---
+++ AwsPoc/src/consumer-event-b/Function.cs
@@ -0,0 +1,23 @@
+using System;
+using Amazon.Lambda.SNSEvents;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace ConsumerEventB
+{
+    public class Function
+    {
+       public string FunctionHandler(SNSEvent snsEvent)
+       {
+           foreach (var record in snsEvent.Records)
+           {
+               var snsRecord = record.Sns;
+               Console.WriteLine($"[{record.EventSource} {snsRecord.Timestamp}] Message = {snsRecord.Message}");
+           }
+           
+           return String.Empty;
+       }
+    }
+}
